a531a3cde63718ff467d14451999cda8dd47ef13
nguyencongbinhphuong12091999@gmail.com <nguyencongbinhphuong12091999@gmail.com>
2021-01-09 02:10:39 +0700

    create admin

--- DjangoStudentManagementSystem/DjangoStudentManagementSystem/settings.py
+++ DjangoStudentManagementSystem/DjangoStudentManagementSystem/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'studentwebapp',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = "studentwebapp.CustomUser"
+
+# Registering Custom Backend "EmailBackEnd"
+AUTHENTICATION_BACKENDS = ['studentwebapp.EmailBackEnd.EmailBackEnd']
--- DjangoStudentManagementSystem/studentwebapp/views.py
+++ DjangoStudentManagementSystem/studentwebapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
  
-from student_management_app.EmailBackEnd import EmailBackEnd
+from studentwebapp.EmailBackEnd import EmailBackEnd
  
  
 def home(request):
